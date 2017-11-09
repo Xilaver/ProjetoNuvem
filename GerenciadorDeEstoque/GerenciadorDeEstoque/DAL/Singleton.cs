257504a74310cfc71629a8ecddfdeafc58ed39e3
Xilaver <xilaversantos@hotmail.com>
2017-11-09 10:58:09 -0200

    Criação do DAL e dos Models

---
+++ GerenciadorDeEstoque/GerenciadorDeEstoque/DAL/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using GerenciadorDeEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GerenciadorDeEstoque.DAL
+{
+    internal class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+        private readonly Entities entities;
+
+        private Singleton()
+        {
+            entities = new Entities();
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public Entities Entities
+        {
+            get
+            {
+                return entities;
+            }
+        }
+    }
+}
