0a8e94f9f3a859e03c68d58529ff10c086be84b0
KauanHonorio123 <kauanhonorio@gmail.com>
2017-11-12 19:34:23 -0200

    Edição Model Produto

--- GerenciadorDeEstoque/GerenciadorDeEstoque/Models/Produto.cs
+++ GerenciadorDeEstoque/GerenciadorDeEstoque/Models/Produto.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace GerenciadorDeEstoque.Models
 {
+    [Table("Produtos")]
     public class Produto
     {
+        [Key]
+        public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [Display(Name = "Nome do Produto")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [Display(Name = "Quantidade de Produto")]
+        public int Quantidade { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [Display(Name = "Nome do Produto")]
+        public Fornecedor Fornecedor { get; set; }
+
+        public List<Categoria> Categorias { get; set; }
+
     }
 }
