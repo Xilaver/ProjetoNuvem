e227095d62fa6ed410c6a9b38e4324b955c2d16e
Xilaver <xilaversantos@hotmail.com>
2017-11-14 10:27:08 -0200

    Criando Controller de produto e fornecedor

--- GerenciadorDeEstoque/GerenciadorDeEstoque/Models/Entities.cs
+++ GerenciadorDeEstoque/GerenciadorDeEstoque/Models/Entities.cs
@@ -10,5 +10,9 @@ namespace GerenciadorDeEstoque.Models
     {
         public DbSet<Empresa> Empresas { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
+
+        public System.Data.Entity.DbSet<GerenciadorDeEstoque.Models.Fornecedor> Fornecedors { get; set; }
+
+        public System.Data.Entity.DbSet<GerenciadorDeEstoque.Models.Produto> Produtoes { get; set; }
     }
 }
