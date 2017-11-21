2be041db474fb7762ce78a44a958576628be2d4a
Xilaver <xilaversantos@hotmail.com>
2017-11-21 09:38:50 -0200

    Alterações em empresa

---
+++ GerenciadorDeEstoque/GerenciadorDeEstoque/Controllers/EmpresasController.cs
@@ -0,0 +1,29 @@
+﻿using GerenciadorDeEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GerenciadorDeEstoque.Controllers
+{
+    public class EmpresasController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Empresas
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Cnpj,Telefone,Email,Login,Senha")] Empresa empresa)
+        {
+            db.Empresas.Add(empresa);
+            db.SaveChanges();
+            return RedirectToAction("Index","Login");
+        }
+
+    }
+}
